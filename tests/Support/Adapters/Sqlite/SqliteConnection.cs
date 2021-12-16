67ccc1a994d5dd8a2a2094e9ab3117a3154733af
Yamithorn <alvarezyansi@gmail.com>
2021-12-16 10:16:26 -0500

    Remove reference

--- tests/Support/Adapters/Sqlite/SqliteConnection.cs
+++ tests/Support/Adapters/Sqlite/SqliteConnection.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Data.SQLite;
-using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Transactions;
